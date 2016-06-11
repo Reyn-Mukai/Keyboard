89ab603084a0b241cf9313a044f327971289f612
OblivionShadow <AbyssalShadow13@gmail.com>
2016-06-10 16:37:22 -1000

    Remapped physical pins
    Changed LED's to PWM capable pins and moved LED flashing to startup.

--- Keyboard.ino
+++ Keyboard.ino
@@ -1,23 +1,23 @@
 #include "Keyboard.h"
 
 //Defines/Macros
-#define XLED A2
-#define ZLED A3
 #define ZBUTTON 2
 #define XBUTTON 3
 #define LABUTTON 4
-#define DABUTTON 5
-#define RABUTTON 6
+#define XLED 5
+#define ZLED 6
 #define UABUTTON 7
 #define ENTBUTTON 8
 #define ESCBUTTON 9
 #define KBSWITCH 10
+#define RABUTTON 14
+#define DABUTTON 15
 #define ACTIVE 16
 
 //Variable Declarations
 int zflag, xflag, laflag, daflag, raflag, uaflag, entflag, escflag = 0;
 int zdeb, xdeb, ladeb, dadeb, radeb, uadeb, entdeb, escdeb = 0;
-int kbmode = 0;
+//int kbmode = 0;
 
 void setup() {
 
@@ -31,11 +31,39 @@ void setup() {
   pinMode(ENTBUTTON, INPUT_PULLUP);
   pinMode(ESCBUTTON, INPUT_PULLUP);
   pinMode(KBSWITCH, INPUT_PULLUP);
+  pinMode(ZLED, OUTPUT);
+  pinMode(XLED, OUTPUT);
   pinMode(ACTIVE, OUTPUT);
 
 
   Serial.begin(115200);
   Serial.println("Pins initialized.");
+
+  delay(16);
+  digitalWrite(ACTIVE, HIGH);
+  analogWrite(ZLED, 100);
+  analogWrite(XLED, 100);
+  delay(13);
+  digitalWrite(ACTIVE, LOW);
+  analogWrite(ZLED, 0);
+  analogWrite(XLED, 0);
+  delay(16);
+  digitalWrite(ACTIVE, HIGH);
+  analogWrite(ZLED, 100);
+  analogWrite(XLED, 100);
+  delay(13);
+  digitalWrite(ACTIVE, LOW);
+  analogWrite(ZLED, 0);
+  analogWrite(XLED, 0);
+  delay(16);
+  digitalWrite(ACTIVE, HIGH);
+  analogWrite(ZLED, 100);
+  analogWrite(XLED, 100);
+  delay(13);
+  digitalWrite(ACTIVE, LOW);
+  analogWrite(ZLED, 0);
+  analogWrite(XLED, 0);
+  delay(16);
 }
 
 void loop() {
@@ -52,9 +80,9 @@ void loop() {
     digitalWrite(ACTIVE, HIGH);
     if(digitalRead(ZBUTTON) == LOW && zflag == 0){
       Keyboard.press(122);
-      analogWrite(ZLED, 150);
+      analogWrite(ZLED, 100);
       zflag = 1;
-    } 
+    }
     if(digitalRead(ZBUTTON) == HIGH && zflag == 1){
       Keyboard.release(122);
       analogWrite(ZLED, 0);
@@ -62,7 +90,7 @@ void loop() {
     }
     if(digitalRead(XBUTTON) == LOW && xflag == 0){
       Keyboard.press(120);
-      analogWrite(XLED, 150);
+      analogWrite(XLED, 100);
       xflag = 1;
     }
     if(digitalRead(XBUTTON) == HIGH && xflag == 1){
@@ -118,7 +146,7 @@ void loop() {
       Keyboard.release(0xB1);
       escflag = 0;
     }
-    kbmode = 0;
+    //kbmode = 0;
   }
 
   Keyboard.end();
@@ -127,38 +155,41 @@ void loop() {
 
   //Releases keys if buttons are pressed while mode is switched
   if(xflag == 1 || zflag == 1 || laflag == 1 || daflag == 1 || raflag == 1 || uaflag == 1 || escflag == 1 || entflag == 1){
+    xflag = zflag = laflag = daflag = raflag = uaflag = escflag = entflag = 0;
     Keyboard.releaseAll();
   }
 
   //Temporary LED flashing
-  if(kbmode == 0){
+  /*if(kbmode == 0){
     delay(16);
     digitalWrite(ACTIVE, HIGH);
-    analogWrite(ZLED, 150);
-    analogWrite(XLED, 150);
+    analogWrite(ZLED, 100);
+    analogWrite(XLED, 100);
     delay(13);
     digitalWrite(ACTIVE, LOW);
     analogWrite(ZLED, 0);
     analogWrite(XLED, 0);
     delay(16);
     digitalWrite(ACTIVE, HIGH);
-    analogWrite(ZLED, 150);
-    analogWrite(XLED, 150);
+    analogWrite(ZLED, 100);
+    analogWrite(XLED, 100);
     delay(13);
     digitalWrite(ACTIVE, LOW);
     analogWrite(ZLED, 0);
     analogWrite(XLED, 0);
     delay(16);
     digitalWrite(ACTIVE, HIGH);
-    analogWrite(ZLED, 150);
-    analogWrite(XLED, 150);
+    analogWrite(ZLED, 100);
+    analogWrite(XLED, 100);
     delay(13);
     digitalWrite(ACTIVE, LOW);
     analogWrite(ZLED, 0);
     analogWrite(XLED, 0);
     delay(16);
     kbmode = 1;
-  }
+  }*/
+
+  //exit1:
 
   //Debug serial & button flashing
   if(digitalRead(ZBUTTON) == LOW && zdeb == 0){
