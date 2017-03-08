eacafa4db6e74bfbda41184a720316be4f7f1a3b
Reyn-Mukai <reyn.mukai@gmail.com>
2017-03-08 01:26:43 -1000

    Implemented general functions & software debounce

--- Keyboard.ino
+++ Keyboard.ino
@@ -1,26 +1,40 @@
-#include "Keyboard.h"
+#include <Keyboard.h>
 
-//Defines/Macros
+//Debounce Length (ms)
+#define debounceMain 10
+#define debounceSecondary 10
+
+//Keybindings
+#define xchar 0x78
+#define zchar 0x7A
+#define lachar 0xD8
+#define dachar 0xD9
+#define rachar 0xD7
+#define uachar 0xDA
+#define entchar 0xB0
+#define escchar 0xB1
+
 #define ZBUTTON 2
 #define XBUTTON 3
-#define LABUTTON 4
+#define LABUTTON 7
 #define XLED 5
 #define ZLED 6
-#define UABUTTON 7
-#define ENTBUTTON 8
-#define ESCBUTTON 9
+#define UABUTTON 9
+#define ENTBUTTON 15
+#define ESCBUTTON 14
 #define KBSWITCH 10
-#define RABUTTON 14
-#define DABUTTON 15
-#define ACTIVE 16
+#define RABUTTON A0
+#define DABUTTON 8
+#define ACTIVELED 16
 #define TRUE 1
 #define FALSE 0
 
 //Variable Declarations
-int zflag, xflag, laflag, daflag, raflag, uaflag, entflag, escflag = 0;
-int zdeb, xdeb, ladeb, dadeb, radeb, uadeb, entdeb, escdeb = 0;
-//Software to be implemented
-int HWDebounce = TRUE; //Set HWDebounce to true if hardware based debounce is implemented otherwise false
+volatile int xflag, zflag = 0;
+volatile unsigned long xtimer, ztimer = 0;
+int laflag, daflag, raflag, uaflag, entflag, escflag = 0;
+unsigned long latimer, datimer, ratimer, uatimer, enttimer, esctimer = 0;
+int active = 0;
 
 void setup() {
 
@@ -36,201 +50,181 @@ void setup() {
   pinMode(KBSWITCH, INPUT_PULLUP);
   pinMode(ZLED, OUTPUT);
   pinMode(XLED, OUTPUT);
-  pinMode(ACTIVE, OUTPUT);
+  pinMode(ACTIVELED, OUTPUT);
 
+  //attachInterrupt(digitalPinToInterrupt(XBUTTON), kbPressX, CHANGE);
+  //attachInterrupt(digitalPinToInterrupt(ZBUTTON), kbPressZ, CHANGE);
+
+  clockTest();
 
   Serial.begin(115200);
   Serial.println("Pins initialized.");
-
-  delay(16);
-  digitalWrite(ACTIVE, HIGH);
-  analogWrite(ZLED, 100);
-  analogWrite(XLED, 100);
-  delay(13);
-  digitalWrite(ACTIVE, LOW);
-  analogWrite(ZLED, 0);
-  analogWrite(XLED, 0);
-  delay(16);
-  digitalWrite(ACTIVE, HIGH);
-  analogWrite(ZLED, 100);
-  analogWrite(XLED, 100);
-  delay(13);
-  digitalWrite(ACTIVE, LOW);
-  analogWrite(ZLED, 0);
-  analogWrite(XLED, 0);
-  delay(16);
-  digitalWrite(ACTIVE, HIGH);
-  analogWrite(ZLED, 100);
-  analogWrite(XLED, 100);
-  delay(13);
-  digitalWrite(ACTIVE, LOW);
-  analogWrite(ZLED, 0);
-  analogWrite(XLED, 0);
-  delay(16);
 }
 
 void loop() {
+  
+  libInit();
+  libTerm();
 
-  //Only begins Keyboard if grounded
-  //Keyboard functions only run within loop
-  //Must be LOW to be able to program the stand alone board
-  if(digitalRead(KBSWITCH) == LOW){
-    Keyboard.begin();
+  while(digitalRead(KBSWITCH) == LOW){
+    digitalWrite(ACTIVELED, HIGH);
+    kbPress(LABUTTON, NULL, lachar, &laflag, &latimer);
+    kbPress(DABUTTON, NULL, dachar, &daflag, &datimer);
+    kbPress(RABUTTON, NULL, rachar, &raflag, &ratimer);
+    kbPress(UABUTTON, NULL, uachar, &uaflag, &uatimer);
+    kbPress(ESCBUTTON, NULL, escchar, &escflag, &esctimer);
+    kbPress(ENTBUTTON, NULL, entchar, &entflag, &enttimer);
+    kbPress(XBUTTON, XLED, xchar, &xflag, &xtimer);
+    kbPress(ZBUTTON, ZLED, zchar, &zflag, &ztimer);
   }
 
-  //Keyboard input mode
-  while(digitalRead(KBSWITCH) == LOW){
-    digitalWrite(ACTIVE, HIGH);
-    if(digitalRead(ZBUTTON) == LOW && zflag == 0){
-      Keyboard.press(122);
-      analogWrite(ZLED, 100);
-      zflag = 1;
+  kbDeb(LABUTTON, &laflag, &latimer);
+  kbDeb(DABUTTON, &daflag, &datimer);
+  kbDeb(RABUTTON, &raflag, &ratimer);
+  kbDeb(UABUTTON, &uaflag, &uatimer);
+  kbDeb(ENTBUTTON, &entflag, &enttimer);
+  kbDeb(ESCBUTTON, &escflag, &esctimer);
+  kbDeb(XBUTTON, &xflag, &xtimer);
+  kbDeb(ZBUTTON, &zflag, &ztimer);
+}
+
+void libInit(){
+  if(digitalRead(KBSWITCH) == LOW && active == false){
+    active = true;
+    flagReset();
+    digitalWrite(ACTIVELED, HIGH);
+    Keyboard.begin();
+    Serial.println("KB lib active");
+  }
+}
+
+void libTerm(){
+  if(digitalRead(KBSWITCH) == HIGH && active == true){
+    active = false;
+    flagReset();
+    digitalWrite(ACTIVELED, LOW);
+    Keyboard.releaseAll();
+    Keyboard.end();
+    Serial.println("KB lib inactive");
+  }
+}
+
+void flagReset(){
+  laflag=raflag=daflag=uaflag=escflag=entflag=xflag=zflag=0;
+  Serial.println("Flags reset");
+}
+
+void writeAll(int level){
+  digitalWrite(XLED, level);
+  digitalWrite(ZLED, level);
+  digitalWrite(ACTIVELED, level);
+}
+
+void clockTest(){
+  if(digitalRead(XBUTTON) == LOW && digitalRead(ZBUTTON) == LOW){
+    for(int i=0; i<3; i++){
+      writeAll(HIGH);
+      delay(1000);
+      writeAll(LOW);
+      delay(1000);
     }
-    if(digitalRead(ZBUTTON) == HIGH && zflag == 1){
-      Keyboard.release(122);
-      analogWrite(ZLED, 0);
+  }
+}
+/*
+void kbPressX(){
+  if(digitalRead(XBUTTON) == LOW && xflag == 0){
+    if(active == true){
+      Keyboard.press(xchar);
+      analogWrite(XLED, 100);
+    }
+    else{
+      digitalWrite(ACTIVELED, HIGH);
+      Serial.print("Pin: ");
+      Serial.print(XBUTTON);
+      Serial.println(" is LOW");
+    }
+    xtimer = millis();
+    xflag = 1;
+  }
+  if(digitalRead(XBUTTON) == HIGH && xflag == 1 && millis()-xtimer > debounceMain){
+    if(active == true){
+      Keyboard.release(xchar);
+      analogWrite(XLED, 0);
+    }
+    else{
+      digitalWrite(ACTIVELED, LOW);
+      Serial.print("Pin: ");
+      Serial.print(XBUTTON);
+      Serial.println(" is HIGH");
+    }
+    xflag = 0;
+  }
+}
+
+void kbPressZ(){
+  if(digitalRead(ZBUTTON) == LOW && zflag == 0){
+    if(active == true){
+      Keyboard.press(zchar);
+      analogWrite(ZLED, 100);
+    }
+    else{
+      digitalWrite(ACTIVELED, HIGH);
+      Serial.print("Pin: ");
+      Serial.print(ZBUTTON);
+      Serial.println(" is LOW");
+    }
+    ztimer = millis();
+    zflag = 1;
+    if(ztimer == 5 && digitalRead(ZBUTTON) == HIGH){
+      Keyboard.release(zchar);
       zflag = 0;
     }
-    if(digitalRead(XBUTTON) == LOW && xflag == 0){
-      Keyboard.press(120);
-      analogWrite(XLED, 100);
-      xflag = 1;
-    }
-    if(digitalRead(XBUTTON) == HIGH && xflag == 1){
-      Keyboard.release(120);
-      analogWrite(XLED, 0);
-      xflag = 0;
-    }
-    if(digitalRead(LABUTTON) == LOW && laflag == 0){
-      Keyboard.press(0xD8);
-      laflag = 1;
-    }
-    if(digitalRead(LABUTTON) == HIGH && laflag == 1){
-      Keyboard.release(0xD8);
-      laflag = 0;
-    }
-    if(digitalRead(DABUTTON) == LOW && daflag == 0){
-      Keyboard.press(0xD9);
-      daflag = 1;
-    }
-    if(digitalRead(DABUTTON) == HIGH && daflag == 1){
-      Keyboard.release(0xD9);
-      daflag = 0;
-    }
-    if(digitalRead(RABUTTON) == LOW && raflag == 0){
-      Keyboard.press(0xD7);
-      raflag = 1;
-    }
-    if(digitalRead(RABUTTON) == HIGH && raflag == 1){
-      Keyboard.release(0xD7);
-      raflag = 0;
-    }
-    if(digitalRead(UABUTTON) == LOW && uaflag == 0){
-      Keyboard.press(0xDA);
-      uaflag = 1;
-    }
-    if(digitalRead(UABUTTON) == HIGH && uaflag == 1){
-      Keyboard.release(0xDA);
-      uaflag = 0;
-    }
-    if(digitalRead(ENTBUTTON) == LOW && entflag == 0){
-      Keyboard.press(0xB0);
-      entflag = 1;
-    }
-    if(digitalRead(ENTBUTTON) == HIGH && entflag == 1){
-      Keyboard.release(0xB0);
-      entflag = 0;
-    }
-    if(digitalRead(ESCBUTTON) == LOW && escflag == 0){
-      Keyboard.press(0xB1);
-      escflag = 1;
-    }
-    if(digitalRead(ESCBUTTON) == HIGH && escflag == 1){
-      Keyboard.release(0xB1);
-      escflag = 0;
-    }
   }
+  if(digitalRead(ZBUTTON) == HIGH && zflag == 1 && millis()-ztimer > debounceMain){
+    if(active == true){
+      Keyboard.release(zchar);
+      analogWrite(ZLED, 0);
+    }
+    else{
+      digitalWrite(ACTIVELED, LOW);
+      Serial.print("Pin: ");
+      Serial.print(ZBUTTON);
+      Serial.println(" is HIGH");
+    }
+    zflag = 0;
+  }
+}
+*/
+void kbPress(int pin, int led, int key, int *flag, unsigned long *timer){
+  if(digitalRead(pin) == LOW && *flag == 0){
+    digitalWrite(led, HIGH);
+    Keyboard.press(key);
+    *timer = millis();
+    *flag = 1;
+  }
+  if(digitalRead(pin) == HIGH && *flag == 1 && millis()-*timer > debounceSecondary){
+    digitalWrite(led, LOW);
+    Keyboard.release(key);
+    digitalWrite(ACTIVELED, LOW);
+    *flag = 0;
+  }
+}
 
-  Keyboard.end();
-
-  digitalWrite(ACTIVE, LOW);
-
-  //Releases keys if buttons are pressed while mode is switched
-  if(xflag == 1 || zflag == 1 || laflag == 1 || daflag == 1 || raflag == 1 || uaflag == 1 || escflag == 1 || entflag == 1){
-    xflag = zflag = laflag = daflag = raflag = uaflag = escflag = entflag = 0;
-    Keyboard.releaseAll();
+void kbDeb(int pin, int *flag, unsigned long *timer){
+  if(digitalRead(pin) == LOW && *flag == 0){
+    Serial.print("Pin: ");
+    Serial.print(pin);
+    Serial.println(" is LOW");
+    digitalWrite(ACTIVELED, HIGH);
+    *timer = millis();
+    *flag = 1;
   }
-
-  //Debug serial & button flashing
-  if(digitalRead(ZBUTTON) == LOW && zdeb == 0){
-    digitalWrite(ACTIVE, HIGH);
-    Serial.println("Z button pressed.");
-    zdeb = 1;
-  }
-  if(digitalRead(ZBUTTON) == HIGH && zdeb == 1){
-    zdeb = 0;
-  }
-  if(digitalRead(XBUTTON) == LOW && xdeb == 0){
-    digitalWrite(ACTIVE, HIGH);
-    Serial.println("X button pressed.");
-    xdeb = 1;
-  }
-  if(digitalRead(XBUTTON) == HIGH && xdeb == 1){
-    xdeb = 0;
-  }
-  if(digitalRead(LABUTTON) == LOW && ladeb == 0){
-    digitalWrite(ACTIVE, HIGH);
-    Serial.println("Left button pressed.");
-    ladeb = 1;
-  }
-  if(digitalRead(LABUTTON) == HIGH && ladeb == 1){
-    ladeb = 0;
-  }
-  if(digitalRead(DABUTTON) == LOW && dadeb == 0){
-    digitalWrite(ACTIVE, HIGH);
-    Serial.println("Down button pressed.");
-    dadeb = 1;
-  }
-  if(digitalRead(DABUTTON) == HIGH && dadeb == 1){
-    dadeb = 0;
-  }
-  if(digitalRead(RABUTTON) == LOW && radeb == 0){
-    digitalWrite(ACTIVE, HIGH);
-    Serial.println("Right button pressed.");
-    radeb = 1;
-  }
-  if(digitalRead(RABUTTON) == HIGH && radeb == 1){
-    radeb = 0;
-  }
-  if(digitalRead(UABUTTON) == LOW && uadeb == 0){
-    digitalWrite(ACTIVE, HIGH);
-    Serial.println("Up button pressed.");
-    uadeb = 1;
-  }
-  if(digitalRead(UABUTTON) == HIGH && uadeb == 1){
-    uadeb = 0;
-  }
-  if(digitalRead(ENTBUTTON) == LOW && entdeb == 0){
-    digitalWrite(ACTIVE, HIGH);
-    Serial.println("Enter button pressed.");
-    entdeb = 1;
-  }
-  if(digitalRead(ENTBUTTON) == HIGH && entdeb == 1){
-    entdeb = 0;
-  }
-  if(digitalRead(ESCBUTTON) == LOW && escdeb == 0){
-    digitalWrite(ACTIVE, HIGH);
-    Serial.println("Escape button pressed.");
-    escdeb = 1;
-  }
-  if(digitalRead(ESCBUTTON) == HIGH && escdeb == 1){
-    escdeb = 0;
-  }
-  if(zdeb == 1 || xdeb == 1 || ladeb == 1 || dadeb == 1 || radeb == 1 || uadeb == 1 || entdeb == 1 || escdeb == 1){
-    digitalWrite(ACTIVE, HIGH);
-  }
-  else{
-    digitalWrite(ACTIVE, LOW);
+  if(digitalRead(pin) == HIGH && *flag == 1 && millis()-*timer > debounceSecondary){
+    Serial.print("Pin: ");
+    Serial.print(pin);
+    Serial.println(" is HIGH");
+    digitalWrite(ACTIVELED, LOW);
+    *flag = 0;
   }
 }
 
